d71dfd854fe3f77e0d99ca096682afe08f0b1de0
lailson henrique <lailson.pi@gmail.com>
2019-08-06 09:04:43 -0300

    acpitulo 02

---
+++ slide01.py
@@ -0,0 +1,28 @@
+##Exercicio 2.1 Converter as expressões matemática no interpretador python 
+print(10 + 20 * 30)
+print(4**2 + 30)
+print( (9**4 +2) * 6 - 1 )
+
+##Exercício 2.2 Expressão matemática
+print( 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
+
+##Exercício 2.3 Faça um programa que exiba seu nome na tela
+print("Lailson Henrique")
+
+##Exercício 2.4 Escreva um programa que exiba o resultado de 2a x 3b, sendo a = 3 e b = 5
+a = 3
+b = 5
+print(3*a * 3*b)
+
+##Exercício 2.5 Modifique o programa 2.7 para calcular a soma de três variaveis
+a = 1
+b = 2
+c = 3
+soma = a + b + c
+print(soma)
+
+##Exercício 2.6 Modifique o programa da lisatgem 2.11 par calcular o aumento de 15% para um salário de  R$ 750,00
+salario = 750
+aumento = 0,15
+total = salario + salario * aumento
+ 
