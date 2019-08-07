67e8d222426cd51a9df0dc626121b14692e8e2fc
lailson henrique <lailson.pi@gmail.com>
2019-08-07 20:55:04 -0300

    Exercicio 12

---
+++ 03-12.py
@@ -0,0 +1,8 @@
+## 3.12. Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+distancia = float(input("Digite a distância em Km: "))
+velocidade = float(input("Digite a velocidade em Km/h: "))
+
+tempo_em_minutos = (distancia / velocidade) * 60
+horas = int(tempo_em_minutos // 60)
+minutos = int(tempo_em_minutos % 60)
+print(f"O tempo estimado da viagem é {horas} horas e {minutos} minutos")
