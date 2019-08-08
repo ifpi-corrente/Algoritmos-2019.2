1ce932c2e3a4d946e2f59e9488df23b1e25f7fe2
lailson henrique <lailson.pi@gmail.com>
2019-08-07 21:25:00 -0300

    temperatura

---
+++ 03-13.py
@@ -0,0 +1,5 @@
+##3.13. Escreva um programa que converta uma temperatura digitada em C para F. A fórmula para a conversão é:
+c = float(input("Digite a temperatura em celcius: "))
+f = ((9 * c ) / 5 ) + 32
+
+print(f'A temperatura em fahrenheit é {f}')
