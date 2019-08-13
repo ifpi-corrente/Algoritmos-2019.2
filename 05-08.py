f8b1fcb77a55be99590473208ed90675a0b2e0b0
lailson henrique <lailson.pi@gmail.com>
2019-08-13 08:43:28 -0300

    while

---
+++ 05-08.py
@@ -0,0 +1,6 @@
+#5.8. Crie um programa que imprima os números pares de 0 até um número digitado sem IF.
+x = int(input("Digite um número limite: "))
+y=0
+while y <= x:
+    print(y)
+    y+=2
---
+++ tabuada.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número: "))
+x = 0
+while x <= 10:
+    print(f'{numero} x {x} = {numero*x} ')
+    x += 1
