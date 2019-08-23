efbe8388f0d7a8455e7e4ef1663b8306ff0d95bb
lailson henrique <lailson.pi@gmail.com>
2019-08-23 20:20:51 -0300

    aula

---
+++ lista/17.py
@@ -0,0 +1,42 @@
+  #17 - Em uma competição de salto em distância cada atleta tem direito a cinco saltos. 
+#O resultado do atleta será determinado pela média dos cinco valores restantes. 
+#Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo 
+#atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. 
+#O programa deve ser encerrado quando não for informado o nome do atleta. 
+#A saída do programa deve ser conforme o exemplo abaixo:
+nome = 'ifpi'
+atletas=[]
+saltos=[]
+resultados=[]
+
+while len(nome) != 0 :
+    nome=(input("Digite o nome do atleta: "))
+    if len(nome) > 0:
+        atletas.append(nome)
+        soma = 0
+        media = 0
+        for i in range (5):
+            distancia = float(input('Digite o salto: '))
+            saltos.append(distancia)
+            soma = soma + distancia
+        media = soma/5
+        resultados.append(media)       
+
+vezes = len(atletas)
+
+x = 0
+for i in range(vezes):
+    
+    print(f'Atleta: {atletas[i]}')
+    for j in range(5):
+        print(f'Salto: {saltos[x]}')
+        x = x + 1 
+    
+    
+
+#print(atletas)
+#print(saltos)
+#print(resultados)
+            
+    				
+    
