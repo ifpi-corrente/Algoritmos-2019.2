942c9a30e53a1abd4b1231c6d978cbf6e2b540c0
lailson henrique <lailson.pi@gmail.com>
2019-08-24 11:17:56 -0300

    aula

--- lista/19.py
+++ lista/19.py
@@ -1,7 +1,8 @@
-
 #Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da mesma. O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num vetor. Após os dados terem sido completamente informados, o programa deverá calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado pela empresa, e é o seguinte:
 resposta = 1
 resultado = [0,0,0,0,0,0]
+soma = 0
+SO =['Windows Server', 'Unix', 'Linux', 'Netware', 'Mac OS', 'Outro']
 while resposta != 0 :
     print("Qual o melhor Sistema Operacional para uso em servidores?")
   
@@ -16,12 +17,22 @@ while resposta != 0 :
     resposta = int(input("Qual é a resposta? "))
     if resposta >0 and resposta < 7:
         resultado[resposta-1] = resultado[resposta-1] + 1 
-    else:
-        print("Opção inválida!")
-
+        soma += 1
+        
+        
 print(resultado)
-      
-      
+for i in range(6):
+        print(f'{SO[i]} {resultado[i]} {resultado[i]*100/soma:.0f}%')
     
+print('total:',soma)
+
+
+maior = 0 
+indice = 0
+for i in range(6):
+    if resultado[i] > maior :
+        maior = resultado[i]
+        indice = i
     
-    
+
+print(f'O mais votado é o {SO[indice]} que teve {maior} votos')
--- lista/tempCodeRunnerFile.py
+++ lista/tempCodeRunnerFile.py
@@ -1,42 +1,25 @@
-  #17 - Em uma competição de salto em distância cada atleta tem direito a cinco saltos. 
-#O resultado do atleta será determinado pela média dos cinco valores restantes. 
-#Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo 
-#atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. 
-#O programa deve ser encerrado quando não for informado o nome do atleta. 
-#A saída do programa deve ser conforme o exemplo abaixo:
-nome = 'ifpi'
-atletas=[]
-saltos=[]
-resultados=[]
-
-while len(nome) != 0 :
-    nome=(input("Digite o nome do atleta: "))
-    if len(nome) > 0:
-        atletas.append(nome)
-        soma = 0
-        media = 0
-        for i in range (5):
-            distancia = float(input('Digite o salto: '))
-            saltos.append(distancia)
-            soma = soma + distancia
-        media = soma/5
-        resultados.append(media)       
-
-vezes = len(atletas)
-
-x = 0
-for i in range(vezes):
-    
-    print(f'Atleta: {atletas[i]}')
-    for j in range(5):
-        print(f'Salto: {saltos[x]}')
-        x = x + 1 
-    
-    
-
-#print(atletas)
-#print(saltos)
-#print(resultados)
-            
-    				
-    
+#Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da mesma. O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num vetor. Após os dados terem sido completamente informados, o programa deverá calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado pela empresa, e é o seguinte:
+resposta = 1
+resultado = [0,0,0,0,0,0]
+soma = 0
+SO =['Windows Server', 'Unix', 'Linux', 'Netware', 'Mac OS', 'Outro']
+while resposta != 0 :
+    print("Qual o melhor Sistema Operacional para uso em servidores?")
+  
+    print("As possíveis respostas são : ")
+    print("1- Windows Server")
+    print("2- Unix")
+    print("3- Linux")
+    print("4- Netware")
+    print("5- Mac OS")
+    print("6- Outro")
+        
+    resposta = int(input("Qual é a resposta? "))
+    if resposta >0 and resposta < 7:
+        resultado[resposta-1] = resultado[resposta-1] + 1 
+        soma += 1
+        
+        
+print(resultado)
+for i in range(6):
+        print(f'{SO[i]} {resultado[i]} {resultado[i]*100/soma:.0f}')
